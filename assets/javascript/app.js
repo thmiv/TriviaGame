23e6b04e11e36a4da6be3720df568332b6403825
Thomas <thmiv4@gmail.com>
2018-08-02 14:34:24 -0500

    displays qustion

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,45 +1,71 @@
 // trivia game
 var myQuestions = [
     {
-      question: "Who is the cutest?",
+      question: "The answer is a?",
       answers: {
-        a: "Rapty",
-        b: "Poby",
-        c: "Waluigi"
+        a: "aaa",
+        b: "bbb",
+        c: "ccc"
       },
       correctAnswer: "a"
     },
     {
-      question: "What is the best site ever created?",
-      answers: {
-        a: "google",
-        b: "youtube",
-        c: "netflix"
-      },
+      question: "The answer is c?",
+      answers: [
+        "aaaa",
+        "bbbb",
+        "cccc"
+      ],
       correctAnswer: "c"
     },
     {
-      question: "Where is Waldo really?",
+      question: "The answer is d?",
       answers: {
-        a: "Antarctica",
-        b: "Exploring the Pacific Ocean",
-        c: "Sitting in a tree",
-        d: "Minding his own business, so stop asking"
+        a: "aaaaa",
+        b: "bbbbb",
+        c: "ccccc",
+        d: "ddddd"
       },
       correctAnswer: "d"
     }
   ];
 
-var quizContainer = document.getElementById('quiz');
-var resultsContainer = document.getElementById('results');
-var submitButton = document.getElementById('submit');
+var quizContainer = $("#quiz");
+var resultsContainer = $("#results");
+var index = 0;
+var quizEnd;
 
-function buildQuiz(){}
+function createQuiz() {
+   if (index < myQuestions.length) {
+     quizEnd = false; 
+   } else {
+     quizEnd = true;
+   }
+   if (quizEnd === false) {
+      quizContainer.append(myQuestions[index].question);
+      renderButtons();
+      index++;
+   }
+}
 
-function showResults(){}
+function showResults() {
+// will show the answer
+}
+
+function renderButtons() {
+  $("#buttons-view").empty();
+  for (var i = 0; i < myQuestions.length; i++) {
+    var btn = $("<button>");
+    btn.addClass("quiz-btn");
+
+    btn.attr("data-name", myQuestions[i].answers[j]);
+    btn.text(myQuestions[i].answers);
+    $("#buttons-view").append(btn);
+  }
+}
 
 // display quiz right away
-buildQuiz();
+createQuiz();
 
 // on submit, show results
-submitButton.addEventListener('click', showResults);
+//submitButton.addEventListener('click', showResults);
